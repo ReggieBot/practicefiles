f7d24cd51ced612fac5c3508f6833f3e8c497450
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2024-03-07 21:34:05 -0500

    s

--- largest_smallest.java
+++ largest_smallest.java
@@ -9,28 +9,27 @@ public class largest_smallest {
 
         int size = 5; 
 
-        int[] array = new int[size];
+        int[] numberArray = new int[size];
 
         // user input 
-        System.out.println("Enter " + size + " elements of the array:");
+        System.out.println("Enter the following number of elements:" + size);
 
         for (int i = 0; i < size; i++) {
-            System.out.print("Element " + (i + 1) + ": ");
-            array[i] = scanner.nextInt();
+            numberArray[i] = scanner.nextInt();
         }
 
         // realized in previous assignments I haven't been closing the scanner, which I now know can be a resource leak
         scanner.close();
 
         // This took me a while..
-        int largest = array[0];
-        int smallest = array[0];
+        int largest = numberArray[0];
+        int smallest = numberArray[0];
 
         for (int i = 1; i < size; i++) {
-            if (array[i] > largest) {
-                largest = array[i];
-            } else if (array[i] < smallest) {
-                smallest = array[i];
+            if (numberArray[i] > largest) {
+                largest = numberArray[i];
+            } else if (numberArray[i] < smallest) {
+                smallest = numberArray[i];
             }
         }
 
