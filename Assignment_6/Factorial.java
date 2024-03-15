7a6a7cbb51170eb47c5e07a8e1fc1e90342feceb
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2024-03-14 22:47:50 -0400

    Assignment_6

---
+++ Assignment_6/Factorial.java
@@ -0,0 +1,38 @@
+package Assignment_6;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter a number ");
+        int number = keyboard.nextInt();
+
+        int factorial = factorialCalc(number);
+        
+        System.out.println(factorial);
+    }
+
+
+    // method
+
+    public static int factorialCalc(int number) {
+    // the factorial of 0 is = 1, so I added a check
+        if (number == 0) {
+            return 1;
+        }
+        else {
+
+            int factorial = 1;
+
+            // using for loop to calculate the factorial.
+            // I did this by multiplying the factorial by each number from 1 - user input number
+            for (int i = 1; i <= number; i++) { // continue looping if i <= user number
+                factorial *= i;
+            }
+
+            return factorial; // return the calculated factorial
+        }
+    }
+
+}
