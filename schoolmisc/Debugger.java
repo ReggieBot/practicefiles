7c29ac71cc99bebf0fc627cf280b761e3a237eb7
Reggie Brown <reggiestephenbrown@gmail.com>
2024-04-07 17:11:44 -0400

    debugger test

---
+++ schoolmisc/Debugger.java
@@ -0,0 +1,9 @@
+public class Debugger {
+    public static void main(String[] args) {
+        int[] testArray = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i <= testArray.length; i++) {
+            System.out.println(testArray[i] + " ");
+        }
+    }
+}
