e837336b2f7ced077e052321e221112983bec81f
Reggie Brown <reggiestephenbrown@gmail.com>
2024-04-21 11:00:45 -0400

    closed scanner resource

--- schoolmisc/odd.java
+++ schoolmisc/odd.java
@@ -5,11 +5,12 @@ public class odd {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number to find odd numbers up to that number: ");
         int limit = scanner.nextInt();
-        
+
         for(int number = 0; number <= limit; number++) {
             if (number % 2 == 1) {
                 System.out.println(number);
             }
         }
+        scanner.close();
     }
 }
