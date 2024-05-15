7ca45c2e4fb96e9af88b043e3af8cfab7ad8d0a0
Reggie Brown <reggiestephenbrown@gmail.com>
2024-05-15 13:15:22 -0400

    update to comments

--- personalmisc/oddnumberrr.java
+++ personalmisc/oddnumberrr.java
@@ -1,6 +1,6 @@
-// Program to find odd numbers given a limit of values
+// program to find odd numbers from a user-given value
 // Reggie Brown
-// May 14, 2024
+// May 15, 2024
 
 package personalmisc;
 import java.util.Scanner;
@@ -8,15 +8,18 @@ import java.util.Scanner;
 public class oddnumberrr {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the odd number program homie!");
-        System.out.println("What is the value limit?: ");
+        // get user input for limit value
+        System.out.println("Welcome to the odd number program");
+        System.out.println("What is the limit?");
         int limit = scanner.nextInt();
 
+        // loop to find odd number
         for (int i = 0; i <= limit; i ++) {
             if (i % 2 == 1) {
                 System.out.println(i);
+
             }
         }
         scanner.close();
-    }
+    } 
 }
